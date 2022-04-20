a46cb91096c943d6f7b974319072d5af6dab2ca9
Jan Wielemaker <J.Wielemaker@vu.nl>
2022-04-20 15:38:56 +0200

    ENHANCED: show_coverage/1,2: call-site annotation (sub goals), several new options to control the output.

--- test_cover.pl
+++ test_cover.pl
@@ -3,7 +3,7 @@
     Author:        Jan Wielemaker
     E-mail:        J.Wielemaker@vu.nl
     WWW:           https://www.swi-prolog.org
-    Copyright (c)  2006-2021, University of Amsterdam
+    Copyright (c)  2006-2022, University of Amsterdam
                               VU University Amsterdam
                               SWI-Prolog Solutions b.v.
     All rights reserved.
@@ -48,15 +48,17 @@
 :- autoload(library(lists), [append/3]).
 :- autoload(library(option), [option/2, option/3]).
 :- autoload(library(readutil), [read_line_to_string/2]).
+:- use_module(prolog_breakpoints, []).
 
 :- set_prolog_flag(generate_debug_info, false).
 
 /** <module> Clause coverage analysis
 
 The purpose of this module is to find which part of the program has been
-used by a certain goal. Usage is defined   in terms of clauses that have
-fired, separated in clauses that  succeeded   at  least once and clauses
-that failed on each occasion.
+used by a certain goal. Usage is defined   in terms of clauses for which
+the _head unification_ succeeded. For each clause  we count how often it
+succeeded and how often it  failed.  In   addition  we  track  all _call
+sites_, creating goal-by-goal annotated clauses.
 
 This module relies on the  SWI-Prolog   tracer  hooks. It modifies these
 hooks and collects the results, after   which  it restores the debugging
@@ -75,8 +77,6 @@ are omitted from the result.
         to do 100% complete meta-interpretation of Prolog.  Example
         problem areas include handling cuts in control-structures
         and calls from non-interpreted meta-predicates.
-@tbd    Provide detailed information organised by predicate.  Possibly
-        annotate the source with coverage information.
 */
 
 
@@ -109,10 +109,20 @@ are omitted from the result.
 %       related to a physical line in the file is annotated
 %       with one of:
 %
-%         | ### | Clause was never executed.                       |
-%         | ++N | Clause was executed N times and always succeeded |
-%         | --N | Clause was executed N times and never succeeded  |
-%         | +N-M | Clause was succeeded N times and failed M times |
+%         | ###  | Clause was never executed.                       |
+%         | ++N  | Clause was entered N times and always succeeded  |
+%         | --N  | Clause was entered N times and never succeeded   |
+%         | +N-M | Clause has succeeded N times and failed M times  |
+%         | +N*M | Clause was entered N times and succeeded M times |
+%
+%       All _call sites_ are annotated using the same conventions,
+%       except that `---` is used to annotate subgoals that were
+%       never called.
+%     - line_numbers(Boolean)
+%       If `true` (default), add line numbers to the annotated file.
+%     - color(Boolean)
+%       Controls using ANSI escape sequences to color the output
+%       in the annotated source.  Default is `true`.
 
 show_coverage(Goal) :-
     show_coverage(Goal, []).
@@ -134,7 +144,7 @@ setup_trace(state(Visible, Leash, Ref)) :-
     set_prolog_flag(coverage_analysis, true),
     asserta((user:prolog_trace_interception(Port, Frame, _, continue) :-
                     prolog_cover:assert_cover(Port, Frame)), Ref),
-    port_mask([unify,exit], Mask),
+    port_mask([unify,exit,call], Mask),
     '$visible'(Visible, Mask),
     '$leash'(Leash, Mask),
     trace.
@@ -152,7 +162,10 @@ cleanup_trace(state(Visible, Leash, Ref), Options) :-
     erase(Ref),
     set_prolog_flag(coverage_analysis, false),
     covered(Succeeded, Failed),
-    file_coverage(Succeeded, Failed, Options),
+    (   report_hook(Succeeded, Failed)
+    ->  true
+    ;   file_coverage(Succeeded, Failed, Options)
+    ),
     clean_data.
 
 %!  assert_cover(+Port, +Frame) is det.
@@ -163,17 +176,35 @@ cleanup_trace(state(Visible, Leash, Ref), Options) :-
 
 :- public assert_cover/2.
 
-assert_cover(unify, Frame) :-
-    running_static_pred(Frame),
-    prolog_frame_attribute(Frame, clause, Cl),
+assert_cover(call, Frame) =>
+    (   call_site(Frame, CallSite)
+    ->  assert_event(cover_enter, CallSite)
+    ;   true
+    ).
+assert_cover(unify, Frame) =>
+    (   prolog_frame_attribute(Frame, clause, Cl),
+        running_static_pred(Frame)
+    ->  assert_event(cover_enter, Cl)
+    ;   true
+    ).
+assert_cover(exit, Frame) =>
     !,
-    assert_entered(Cl).
-assert_cover(exit, Frame) :-
-    running_static_pred(Frame),
-    prolog_frame_attribute(Frame, clause, Cl),
-    !,
-    assert_exited(Cl).
-assert_cover(_, _).
+    (   prolog_frame_attribute(Frame, clause, Cl),
+        running_static_pred(Frame)
+    ->  assert_event(cover_exits, Cl)
+    ;   true
+    ),
+    (   call_site(Frame, CallSite)
+    ->  assert_event(cover_exits, CallSite)
+    ;   true
+    ).
+
+call_site(Frame, call_site(Clause,PC,PI)) :-
+    prolog_frame_attribute(Frame, pc, PC),
+    prolog_frame_attribute(Frame, predicate_indicator, PI),
+    prolog_frame_attribute(Frame, parent, Parent),
+    running_static_pred(Parent),
+    prolog_frame_attribute(Parent, clause, Clause).
 
 %!  running_static_pred(+Frame) is semidet.
 %
@@ -182,20 +213,15 @@ assert_cover(_, _).
 running_static_pred(Frame) :-
     prolog_frame_attribute(Frame, goal, Goal),
     \+ '$get_predicate_attribute'(Goal, (dynamic), 1).
-%   \+ predicate_property(Goal, dynamic).
 
-%!  assert_entered(+Ref) is det.
-%!  assert_exited(+Ref) is det.
+%!  assert_event(+Event, +Object) is det.
 %
-%   Add Ref to the set of entered or exited clauses.
+%   Count a new event on Object.  Currently   Object  is either a clause
+%   reference of a term call_site(Clause,PC).
 
-assert_entered(Cl) :-
-    add_clause(Cl, I),
-    bump(cover_enter, I).
-
-assert_exited(Cl) :-
-    add_clause(Cl, I),
-    bump(cover_exits, I).
+assert_event(Event, Object) :-
+    add_clause(Object, I),
+    bump(Event, I).
 
 bump(Var, I) :-
     nb_getval(Var, Array),
@@ -204,16 +230,16 @@ bump(Var, I) :-
     nb_setarg(I, Array, New).
 
 :- dynamic
-    clause_index/2.
+    object_index/2.
 
 add_clause(Cl, I) :-
-    clause_index(Cl, I),
+    object_index(Cl, I),
     !.
 add_clause(Cl, I) :-
     nb_getval(cover_count, I0),
     I is I0+1,
     nb_setval(cover_count, I),
-    assertz(clause_index(Cl, I)),
+    assertz(object_index(Cl, I)),
     expand_arrays(I).
 
 expand_arrays(I) :-
@@ -256,16 +282,20 @@ clean_data :-
     nb_delete(cover_count),
     nb_delete(cover_enter),
     nb_delete(cover_exits),
-    retractall(clause_index(_,_)).
+    retractall(object_index(_,_)).
 
 %!  count(+Which, +Clause, -Count) is semidet.
 %
 %   Get event counts for Clause.
 
 count(Which, Cl, Count) :-
-    clause_index(Cl, I),
+    object_index(Cl, I),
+    counter(Which),
     nb_getval(Which, Array),
     arg(I, Array, Count).
+
+counter(cover_enter).
+counter(cover_exits).
 
 entered(Cl) :-
     count(cover_enter, Cl, Count),
@@ -349,11 +379,11 @@ clean_set(Clauses, UserClauses) :-
     exclude(is_system_clause, Clauses_wo_pldoc, UserClauses).
 
 is_system_clause(Clause) :-
-    clause_name(Clause, Name),
+    clause_pi(Clause, Name),
     Name = system:_.
 
 is_pldoc(Clause) :-
-    clause_name(Clause, _Module:Name2/_Arity),
+    clause_pi(Clause, _Module:Name2/_Arity),
     pldoc_predicate(Name2).
 
 pldoc_predicate('$pldoc').
@@ -413,13 +443,17 @@ annotate_file(Options) :-
     ),
     !.
 
-%! detailed_report(+Uncovered, +Covered, +File:atom, +Options) is det
+%!  detailed_report(+Uncovered, +Covered, +File:atom, +Options) is det
+%
+%   @arg Uncovered is a list of uncovered clauses
+%   @arg Covered is a list of covered clauses
 
 detailed_report(Uncovered, Covered, File, Options):-
     annotate_file(Options),
     !,
     convlist(line_annotation(File, uncovered), Uncovered, Annot1),
-    convlist(line_annotation(File, covered),   Covered,   Annot2),
+    convlist(line_annotation(File, covered),   Covered,   Annot20),
+    flatten(Annot20, Annot2),
     append(Annot1, Annot2, AnnotationsLen),
     pairs_keys_values(AnnotationsLen, Annotations, Lens),
     max_list(Lens, MaxLen),
@@ -436,37 +470,49 @@ detailed_report(Uncovered, _, File, _Options):-
     maplist(print_clause_line, Compact_pairs),
     nl.
 
-line_annotation(File, Class, Clause, (Line-Annot)-Len) :-
+line_annotation(File, uncovered, Clause, Annotation) :-
+    !,
     clause_property(Clause, file(File)),
     clause_property(Clause, line_count(Line)),
-    annot(Class, Clause, Annot, Len).
-
-annot(uncovered, _Clause, ansi(error,###), 3).
-annot(covered,    Clause, Annot, Len) :-
+    Annotation = (Line-ansi(error,###))-3.
+line_annotation(File, covered, Clause, [(Line-Annot)-Len|CallSites]) :-
+    clause_property(Clause, file(File)),
+    clause_property(Clause, line_count(Line)),
     entered(Clause, Entered),
     exited(Clause, Exited),
+    counts_annotation(Entered, Exited, Annot, Len),
+    findall(((CSLine-CSAnnot)-CSLen)-PC,
+            clause_call_site_annotation(Clause, PC, CSLine, CSAnnot, CSLen),
+            CallSitesPC),
+    pairs_keys_values(CallSitesPC, CallSites, PCs),
+    check_covered_call_sites(Clause, PCs).
+
+counts_annotation(Entered, Exited, Annot, Len) :-
     (   Exited == Entered
     ->  format(string(Text), '++~D', [Entered]),
         Annot = ansi(comment, Text)
     ;   Exited == 0
     ->  format(string(Text), '--~D', [Entered]),
         Annot = ansi(warning, Text)
-    ;   Failed is Entered - Exited,
+    ;   Exited < Entered
+    ->  Failed is Entered - Exited,
         format(string(Text), '+~D-~D', [Exited, Failed]),
         Annot = ansi(comment, Text)
+    ;   format(string(Text), '+~D*~D', [Entered, Exited]),
+        Annot = ansi(fg(cyan), Text)
     ),
     string_length(Text, Len).
 
 uncovered_clause_line(File, Clause, Name-Line) :-
     clause_property(Clause, file(File)),
-    clause_name(Clause, Name),
+    clause_pi(Clause, Name),
     clause_property(Clause, line_count(Line)).
 
-%!  clause_name(+Clause, -Name) is det.
+%!  clause_pi(+Clause, -Name) is det.
 %
 %   Return the clause predicate indicator as Module:Name/Arity.
 
-clause_name(Clause, Name) :-
+clause_pi(Clause, Name) :-
     clause(Module:Head, _, Clause),
     functor(Head,F,A),
     Name=Module:F/A.
@@ -475,6 +521,70 @@ print_clause_line((Module:Name/Arity)-Lines):-
     term_string(Module:Name, Complete_name),
     summary(Complete_name, 54, SName),
     format('~4|~w~t~59|~p~n', [SName/Arity, Lines]).
+
+
+		 /*******************************
+		 *     LINE LEVEL CALL SITES	*
+		 *******************************/
+
+clause_call_site_annotation(ClauseRef, NextPC, Line, Annot, Len) :-
+    clause_call_site(ClauseRef, PC-NextPC, Line:_LPos),
+    (   count(cover_enter, call_site(ClauseRef, NextPC, PI), Entered),
+        count(cover_exits, call_site(ClauseRef, NextPC, PI), Exited)
+    ->  counts_annotation(Entered, Exited, Annot, Len)
+    ;   '$fetch_vm'(ClauseRef, PC, _, VMI),
+        \+ no_annotate_call_site(VMI)
+    ->  Annot = ansi(error, ---),
+        Len = 3
+    ).
+
+no_annotate_call_site(i_enter).
+no_annotate_call_site(i_exit).
+no_annotate_call_site(i_cut).
+
+
+clause_call_site(ClauseRef, PC-NextPC, Pos) :-
+    clause_info(ClauseRef, File, TermPos, _NameOffset),
+    '$break_pc'(ClauseRef, PC, NextPC),
+    '$clause_term_position'(ClauseRef, NextPC, List),
+    catch(prolog_breakpoints:range(List, TermPos, SubPos), E, true),
+    (   var(E)
+    ->  arg(1, SubPos, A),
+        file_offset_pos(File, A, Pos)
+    ;   print_message(warning, coverage(clause_info(ClauseRef))),
+        fail
+    ).
+
+file_offset_pos(File, A, Line:LPos) :-
+    file_text(File, String),
+    State = start(1, 0),
+    call_nth(sub_string(String, S, _, _, "\n"), NLine),
+    (   S >= A
+    ->  !,
+        State = start(Line, SLine),
+        LPos is A-SLine
+    ;   NS is S+1,
+        NLine1 is NLine+1,
+        nb_setarg(1, State, NLine1),
+        nb_setarg(2, State, NS),
+        fail
+    ).
+
+file_text(File, String) :-
+    setup_call_cleanup(
+        open(File, read, In),
+        read_string(In, _, String),
+        close(In)).
+
+check_covered_call_sites(Clause, Reported) :-
+    findall(PC, (count(_,call_site(Clause,PC,_), Count), Count > 0), Seen),
+    sort(Reported, SReported),
+    sort(Seen, SSeen),
+    ord_subtract(SSeen, SReported, Missed),
+    (   Missed == []
+    ->  true
+    ;   print_message(warning, coverage(unreported_call_sites(Clause, Missed)))
+    ).
 
 
 		 /*******************************
@@ -528,7 +638,10 @@ annotate_file(Source, Annotations, Options) :-
         close(In)).
 
 annotate(In, Out, Annotations, Options) :-
-    set_stream(Out, tty(true)),
+    (   option(color(true), Options, true)
+    ->  set_stream(Out, tty(true))
+    ;   true
+    ),
     annotate(In, Out, Annotations, 0, Options).
 
 annotate(In, Out, Annotations, LineNo0, Options) :-
@@ -536,16 +649,22 @@ annotate(In, Out, Annotations, LineNo0, Options) :-
     (   Line == end_of_file
     ->  true
     ;   succ(LineNo0, LineNo),
-        option(margin(Margin), Options, 4),
-        (   annotation(LineNo, Annotations, Annot, Annotations1)
-        ->  write_annotation(Out, Annot)
-        ;   Annotations1 = Annotations
-        ),
-        format(Out, '~t~*|~s~n', [Margin, Line]),
+        margins(LMargin, CMargin, Options),
+        line_no(LineNo, Out, LMargin),
+        annotations(LineNo, Out, LMargin, Annotations, Annotations1),
+        format(Out, '~t~*|~s~n', [CMargin, Line]),
         annotate(In, Out, Annotations1, LineNo, Options)
     ).
 
-annotation(Line, [Line-Annot|T], Annot, T).
+annotations(Line, Out, LMargin, [Line-Annot|T0], T) :-
+    !,
+    write_annotation(Out, Annot),
+    (   T0 = [Line-_|_]
+    ->  with_output_to(Out, ansi_format(bold, ' \u2bb0~n~t~*|', [LMargin])),
+        annotations(Line, Out, LMargin, T0, T)
+    ;   T = T0
+    ).
+annotations(_, _, _, Annots, Annots).
 
 write_annotation(Out, ansi(Code, Fmt-Args)) =>
     with_output_to(Out, ansi_format(Code, Fmt, Args)).
@@ -556,3 +675,54 @@ write_annotation(Out, Fmt-Args) =>
 write_annotation(Out, Fmt) =>
     format(Out, Fmt, []).
 
+line_no(_, _, 0) :- !.
+line_no(Line, Out, LMargin) :-
+    with_output_to(Out, ansi_format(fg(127,127,127), '~t~d ~*|',
+                                    [Line, LMargin])).
+
+margins(LMargin, Margin, Options) :-
+    option(line_numbers(true), Options, true),
+    !,
+    option(line_number_margin(LMargin), Options, 6),
+    option(margin(AMargin), Options, 4),
+    Margin is LMargin+AMargin.
+margins(0, Margin, Options) :-
+    option(margin(Margin), Options, 4).
+
+%!  report_hook(+Succeeded, +Failed) is semidet.
+%
+%   This hook is called after the data   collection. It is passed a list
+%   of objects that have succeeded as  well   as  a list of objects that
+%   have failed.  The objects are one of
+%
+%     - ClauseRef
+%       The specified clause
+%     - call_site(ClauseRef, PC, PI)
+%       A call was make in ClauseRef at the given program counter to
+%       the predicate indicated by PI.
+
+:- multifile
+    report_hook/2.
+
+
+		 /*******************************
+		 *             MESSAGES		*
+		 *******************************/
+
+:- multifile
+    prolog:message//1.
+
+prolog:message(coverage(clause_info(ClauseRef))) -->
+    [ 'Inconsistent clause info for '-[] ],
+    clause_msg(ClauseRef).
+prolog:message(coverage(unreported_call_sites(ClauseRef, PCList))) -->
+    [ 'Failed to report call sites for '-[] ],
+    clause_msg(ClauseRef),
+    [ nl, '  Missed at these PC offsets: ~p'-[PCList] ].
+
+clause_msg(ClauseRef) -->
+    { clause_pi(ClauseRef, PI),
+      clause_property(ClauseRef, file(File)),
+      clause_property(ClauseRef, line_count(Line))
+    },
+    [ '~p at'-[PI], nl, '  ', url(File:Line) ].
